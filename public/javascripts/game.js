62296aa3d3a0ea4fa2bce16cb58941c92b5396b0
trebek1 <trebek08@gmail.com>
2016-06-12 15:28:15 -0700

    Added tween for button and made tiled background

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -31,6 +31,7 @@ preload.prototype = {
           game.load.setPreloadSprite(loadingBar);
           game.load.image("title", 'assets/sprites/title.png');
           game.load.image("playbutton", "assets/sprites/playbutton.png");
+          game.load.image("backsplash", "assets/sprites/backsplash.png");
      },
      create: function(){
           this.game.state.start("TitleScreen")
@@ -39,12 +40,22 @@ preload.prototype = {
 var titleScreen = function(game){};
 titleScreen.prototype = {
      create: function(){
-          game.stage.backgroundColor = bgColors[game.rnd.between(0,bgColors.length - 1)];
+          //game.stage.backgroundColor = bgColors[game.rnd.between(0,bgColors.length - 1)];
+               
+          var titleBG = game.add.tileSprite(0,0,game.width,game.height,"backsplash");
+          titleBG.tint = bgColors[game.rnd.between(0, bgColors.length-1)];
+
           var title = game.add.image(game.width / 2, 210, "title");
           title.anchor.set(0.5);
           var playButton = game.add.button(game.width / 2, game.height - 150, "playbutton", this.startGame);
           playButton.anchor.set(0.5);
-     },
+          var tween = game.add.tween(playButton).to({
+               width: 220, 
+               height: 220,
+          }, 1500, "Linear", true, 0, -1); 
+          tween.yoyo(true); 
+          },
+     
      startGame: function(){
           game.state.start("PlayGame")
      }
